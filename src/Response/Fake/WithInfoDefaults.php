a465bdfe607d224e0b25c0134bcb672c3be3496d
Kanstantsin Mesnik <haspadar@gmail.com>
2025-09-29 17:48:32 +0300

    feat: Randomize fake response info

--- src/Response/Fake/WithInfoDefaults.php
+++ src/Response/Fake/WithInfoDefaults.php
@@ -7,6 +7,7 @@ namespace Carl\Response\Fake;
 use Carl\Response\CurlInfo;
 use Carl\Response\Response;
 use Override;
+use Random\RandomException;
 
 /**
  * Decorator that ensures a Response always has
@@ -30,6 +31,9 @@ final readonly class WithInfoDefaults implements Response
         return $this->origin->headers();
     }
 
+    /**
+     * @throws RandomException
+     */
     #[Override]
     public function info(): CurlInfo
     {
@@ -37,21 +41,21 @@ final readonly class WithInfoDefaults implements Response
 
         $defaults = [
             'http_code' => 200,
-            'total_time_us' => 0,
-            'namelookup_time' => 0.0,
-            'connect_time' => 0.0,
+            'total_time_us' => random_int(50_000, 500_000),
+            'namelookup_time' => random_int(1, 10) / 10_000,
+            'connect_time' => random_int(10, 50) / 1_000,
             'appconnect_time' => 0.0,
             'pretransfer_time' => 0.0,
-            'starttransfer_time' => 0.0,
+            'starttransfer_time' => random_int(20, 200) / 1_000,
             'redirect_time' => 0.0,
             'redirect_count' => 0,
             'size_download' => 0,
             'size_upload' => 0,
             'speed_download' => 0,
             'speed_upload' => 0,
-            'url' => '',
-            'primary_ip' => '',
-            'content_type' => '',
+            'url' => 'http://localhost/fake',
+            'primary_ip' => '127.0.0.1',
+            'content_type' => 'text/plain; charset=utf-8',
             'redirect_url' => '',
         ];
 
